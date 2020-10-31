31588c6521ad8043f4507a0b4ff5898b0fe99497
Zoran Simic <zsimic@netflix.com>
2020-10-30 17:07:40 -0700

    Ensure setup.py runs only when invoked as main

--- setup.py
+++ setup.py
@@ -1,28 +1,29 @@
 from setuptools import setup
 
 
-setup(
-    name="runez",
-    setup_requires="setupmeta",
-    versioning="dev",
-    author="Zoran Simic zoran@simicweb.com",
-    url="https://github.com/zsimic/runez",
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "Intended Audience :: Developers",
-        "Operating System :: MacOS :: MacOS X",
-        "Operating System :: POSIX",
-        "Operating System :: Unix",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: Implementation :: CPython",
-        "Topic :: Software Development :: Libraries",
-        "Topic :: Utilities"
-    ],
-)
+if __name__ == "__main__":
+    setup(
+        name="runez",
+        setup_requires="setupmeta",
+        versioning="dev",
+        author="Zoran Simic zoran@simicweb.com",
+        url="https://github.com/zsimic/runez",
+        classifiers=[
+            "Development Status :: 4 - Beta",
+            "Intended Audience :: Developers",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: POSIX",
+            "Operating System :: Unix",
+            "Programming Language :: Python",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 2.7",
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3.6",
+            "Programming Language :: Python :: 3.7",
+            "Programming Language :: Python :: 3.8",
+            "Programming Language :: Python :: 3.9",
+            "Programming Language :: Python :: Implementation :: CPython",
+            "Topic :: Software Development :: Libraries",
+            "Topic :: Utilities"
+        ],
+    )
