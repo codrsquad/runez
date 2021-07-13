ddf598163eff164adc91c594ab10b10c1d6257d2
Zoran Simic <zsimic@netflix.com>
2021-07-13 12:29:06 -0700

    Updated urls

--- setup.py
+++ setup.py
@@ -1,30 +1,32 @@
 from setuptools import setup
 
 
-if __name__ == "__main__":
-    setup(
-        name="runez",
-        setup_requires="setupmeta",
-        versioning="dev",
-        author="Zoran Simic zoran@simicweb.com",
-        python_requires='>=3.6',
-        url="https://github.com/codrsquad/runez",
-        classifiers=[
-            "Development Status :: 5 - Production/Stable",
-            "Intended Audience :: Developers",
-            "Operating System :: MacOS",
-            "Operating System :: POSIX",
-            "Operating System :: POSIX :: Linux",
-            "Operating System :: Unix",
-            "Programming Language :: Python",
-            "Programming Language :: Python :: 3",
-            "Programming Language :: Python :: 3.5",
-            "Programming Language :: Python :: 3.6",
-            "Programming Language :: Python :: 3.7",
-            "Programming Language :: Python :: 3.8",
-            "Programming Language :: Python :: 3.9",
-            "Programming Language :: Python :: Implementation :: CPython",
-            "Topic :: Software Development :: Libraries",
-            "Topic :: Utilities",
-        ],
-    )
+setup(
+    name="runez",
+    setup_requires="setupmeta",
+    versioning="dev",
+    author="Zoran Simic zoran@simicweb.com",
+    python_requires='>=3.6',
+    url="https://github.com/codrsquad/runez",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Operating System :: MacOS",
+        "Operating System :: POSIX",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: Unix",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Utilities",
+    ],
+    project_urls={
+        "Documentation": "https://github.com/codrsquad/runez/wiki",
+        "Source": "https://github.com/codrsquad/runez",
+    },
+)
